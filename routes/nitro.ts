5ddf718a6918d5d057ed6342d3648d5d4f7b1882
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-05-12 15:17:59 -0300

    chore: removing reponse header

--- routes/nitro.ts
+++ routes/nitro.ts
@@ -1,4 +1,4 @@
 export default eventHandler((event) => {
-  setResponseHeader(event, "Cache-Control", "public, max-age=604800");
+  // setResponseHeader(event, "Cache-Control", "public, max-age=604800");
   return { nitro: "Is Awesome!" };
 });
